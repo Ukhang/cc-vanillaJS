fa27829b6bf26a4d3481cada3d0d362875b5afbb
Ukhang <ukhangmarma459@gmail.com>
2025-02-08 17:01:27 +0600

    project setup

--- src/characterAnimation.js
+++ src/characterAnimation.js
@@ -2,5 +2,10 @@ function main() {
     myCanvas.width = window.innerWidth;
     myCanvas.height = window.innerHeight;
 
-    
+    const pumpkin = new Pumpkin(
+        myCanvas.width/2,
+        myCanvas.height/2,
+        Math.min(myCanvas.width, myCanvas.height)*0.5
+    )
+    pumpkin.draw(myCanvas.getContext("2d"));
 }
