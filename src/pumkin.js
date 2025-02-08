dc7280c33788a251f15acfb78e1e9c1b341e31f4
Ukhang <ukhangmarma459@gmail.com>
2025-02-08 17:23:51 +0600

    drawing the thead

--- src/pumkin.js
+++ src/pumkin.js
@@ -6,15 +6,41 @@ class Pumpkin {
     }
 
     draw(ctx) {
-        this.save();
+        ctx.save();
         ctx.translate(this.x, this.y);
         ctx.scale(this.rad, this.rad);
 
-        ctx.fillStyle = "gray";
+        this.#drawHead(ctx);
+
+        ctx.restore();
+    }
+
+    #drawHead(ctx) {
+        ctx.fillStyle = "green";
         ctx.beginPath();
-        ctx.rect(-1, -1, 2, 2);
+        ctx.rect(-0.05, -1, 0.1, 0.1);
         ctx.fill();
-        
-        this.restore();
+
+        ctx.fillStyle="rgb(255,150,0)";
+        ctx.beginPath();
+        ctx.ellipse(-0.6, 0.03, 0.4, 0.92, 0, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.ellipse(0.6, 0.03, 0.4, 0.93, 0, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.fillStyle="rgb(255,170,0)";
+        ctx.beginPath();
+        ctx.ellipse(-0.3, 0.03, 0.4, 0.95, 0, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.ellipse(0.3, 0.03, 0.4, 0.95, 0, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.fillStyle="rgb(255,190,0)";
+        ctx.beginPath();
+        ctx.ellipse(0, 0.03, 0.4, 0.97, 0, 0, Math.PI * 2);
+        ctx.fill();
+
     }
 }
